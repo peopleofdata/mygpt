4199ee91fd1b47d07bc53da31e8eb3e1a71d6b9a
Person <Oskar.Person@rabobank.com>
2023-08-31 11:45:13 +0200

    Simplified ;)

---
+++ apikey.py
@@ -0,0 +1,3 @@
+import os
+apikey = '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'
+os.environ['OPENAI_API_KEY'] = apikey
--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os, openai
+import os, openai, apikey
 from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__, static_folder=".")
-openai.api_key = os.environ.get('openai')
-history = [{'role':'user','content':'Hi! You are in default mode: (C)heck, (M)emorize or (R)ecall or just chat!'}]
-mode = 'default'
-phrases = {'hello':{'translation':'hello','score':0}}
-storage_file = 'memory.pickle'
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+instruction = 'You are a helpful assistant.'
+history = [{'role':'system','content':f'{instruction}'}]
 
 @app.route('/')
 def index():
@@ -17,55 +15,32 @@ def get_history():
     global history
     return jsonify({"history": history}), 200
 
-def query_openai(prompt, text):
+def process_history(history, limit):
+    if len(history)<limit:
+        n=len(history)
+    else:
+        n=limit
+    return history[-n:]
+
+def query_openai():
+    global history
     response = openai.ChatCompletion.create(
-    model="gpt-4",
+    model="gpt-3.5-turbo",
     messages=[
-            {"role": "system", "content": prompt},
-            {"role": "user", "content": f"{text}"}
+            process_history(history, 5)
         ]
     )
-    return response['choices'][0]['message']['content']
+    openai_response = response['choices'][0]['message']
+    return openai_response['content']
 
 @app.route('/store_text', methods=['POST'])
 def store_text():
     global history
-    global mode
-    global phrases
+    global instruction
     text = request.form.get('text')
-    history.append({'role':'user','content': text})
-    if not text:
-        system_response = {'role':'shell', 'content': 'No content?'}
-    if text.lower() in ['e','E','exit','d','default','q']:
-        mode = 'default'
-        system_response = {'role':'shell', 'content': 'Exited to default.'}
-    elif mode == 'default':
-        if text.lower() in ['c','C','check']:
-            system_response = {'role':'shell','content':str(phrases)}
-        elif text.lower() in ['m','M']:
-            mode = 'memorization'
-            system_response = {'role':'shell','content':mode}
-        elif text.lower() in ['r','R']:
-            mode = 'recall'
-            system_response = {'role':'shell','content':mode}
-        else:
-            system_response = {'role':'assistant','content':query_openai("Respond smart and short.", text)}
-    elif mode =='memorization':
-        try:
-            openai_response = query_openai("You translate user input into english", text)
-            phrases[text] = {"translation": openai_response, "score": 0}
-            system_response= {'role':'assistant','content':openai_response}
-        except Exception as e:
-            system_response = {'role':'shell', 'content':str(e)}
-    elif mode =='recall':
-        print('recall_mode')
-        sorted_phrases = sorted(phrases.items(), key=lambda x: x[1]['score'], reverse=True)
-        for phrase, info in sorted_phrases[0]:
-            system_response = {'role':'shell', 'content':phrase}
-            history.append(system_response)
-            prompt = "You respond only CORRECT or INCORRECT. You receive USER input and TRANSLATION and respond CORRECT if the meaning is the same but the language is different."
-            openai_response = query_openai(prompt, f'USER:{text}, TRANSLATION:{phrase}')
-            system_response = {'role':'assistant', 'content':openai_response}
+    user_message = {"role": "user", "content": text}
+    history.append(user_message)
+    system_response = {'role':'assistant','content':query_openai()}
     history.append(system_response)
     return jsonify({"response": "This doesn't work anyway"}), 200
 
